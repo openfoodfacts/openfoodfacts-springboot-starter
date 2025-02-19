0a739fd702f661eda7156c51662a734796d9566b
alpermulayim <alper.mulayim48@gmail.com>
2025-02-19 22:12:49 +0300

    first commit project init

---
+++ openfoodfacts-spring-boot-starter/src/test/java/com/alpermulayim/openfoodfacts_spring_boot_starter/OpenfoodfactsSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpermulayim.openfoodfacts_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfoodfactsSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
