cad13c880021996dc67fd6700d99983b9652e04a
alpermulayim <alper.mulayim48@gmail.com>
2025-02-20 17:31:59 +0300

    DEMO_APP: Demo application for openfoodfacts starter project added

---
+++ openfoodfacts-starter-demo/src/test/java/com/alpermulayim/openfoodfacts_starter_demo/OpenfoodfactsStarterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpermulayim.openfoodfacts_starter_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfoodfactsStarterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
