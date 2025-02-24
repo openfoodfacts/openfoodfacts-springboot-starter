074e558d8ee8b068839bc5d33a9231041d80a04f
alpermulayim <alper.mulayim48@gmail.com>
2025-02-24 16:59:56 +0300

    [DEMO_APP]  Users can set own openfoodfacts webclient properties such as base url. openfoodfacts:   base-url: "https://world.openfoodfacts.net"   search-path: "/api/v2/search"   product-path: "/api/v2/product"

--- openfoodfacts-starter-demo/src/main/java/com/alpermulayim/openfoodfacts_starter_demo/DemoConfig.java
+++ openfoodfacts-starter-demo/src/main/java/com/alpermulayim/openfoodfacts_starter_demo/DemoConfig.java
@@ -1,6 +1,9 @@
 package com.alpermulayim.openfoodfacts_starter_demo;
 
 import com.alpermulayim.openfoodfacts_spring_boot_starter.OpenFoodFactsWebClient;
+import com.alpermulayim.openfoodfacts_spring_boot_starter.config.OpenFoodFactsWebClientConfiguration;
+import com.alpermulayim.openfoodfacts_spring_boot_starter.config.OpenFoodFactsWebClientProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 /**
@@ -9,8 +12,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DemoConfig {
+
+    OpenFoodFactsWebClientProperties properties;
+
+    @Autowired
+    public DemoConfig(OpenFoodFactsWebClientProperties properties) {
+        this.properties = properties;
+    }
+
+
     @Bean
     OpenFoodFactsWebClient openFoodFactsWebClient(){
-        return new OpenFoodFactsWebClient();
+        return new OpenFoodFactsWebClient(properties);
     }
 }
--- openfoodfacts-starter-demo/src/main/java/com/alpermulayim/openfoodfacts_starter_demo/OpenfoodfactsStarterDemoApplication.java
+++ openfoodfacts-starter-demo/src/main/java/com/alpermulayim/openfoodfacts_starter_demo/OpenfoodfactsStarterDemoApplication.java
@@ -1,10 +1,13 @@
 package com.alpermulayim.openfoodfacts_starter_demo;
 
 
+import com.alpermulayim.openfoodfacts_spring_boot_starter.config.OpenFoodFactsWebClientProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OpenFoodFactsWebClientProperties.class)
 public class OpenfoodfactsStarterDemoApplication {
 
     public static void main(String[] args) {
