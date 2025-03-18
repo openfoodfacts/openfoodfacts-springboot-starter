0f8d1f0f794b50d8ac79b4787a8f4d383351e1f7
alpermulayim <alper.mulayim48@gmail.com>
2025-03-18 18:46:15 +0300

    updated version to 0.0.2.7

--- docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.alpermulayim.openfoodfacts_starter_demo"},{"l":"com.alpermulayim.openfoodfacts_starter_demo.dtos"}];updateSearchResults();
